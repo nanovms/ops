d4772d87f999faf26481e2e435faaba5d0e547df
tijoytom <tijoy@nanovms.com>
2018-09-11 16:43:54 -0700

    Download images from cloud

--- nvm_test.go
+++ nvm_test.go
@@ -1,5 +1,34 @@
 package main
-import ("testing")
+import ("testing"
+        "os"
+        "fmt"
+)
+
+func TestDownloadImages(t *testing.T) {
+    // remove the files to force a download
+    // ignore any error from remove
+    os.Remove("mkfs")
+    os.Remove("staging/boot")
+    os.Remove("staging/stage3")
+    downloadImages()
+
+    if _, err := os.Stat("staging/boot"); os.IsNotExist(err) {
+        t.Errorf("staging/boot file not found")
+    }
+    
+    if info, err := os.Stat("mkfs"); os.IsNotExist(err) {
+        t.Errorf("mkfs not found")
+    } else {
+        mode := fmt.Sprintf("%04o", info.Mode().Perm())
+        if mode != "0755" {
+            t.Errorf("mkfs not executable")
+        }
+    }
+    
+    if _, err := os.Stat("staging/stage3"); os.IsNotExist(err) {
+        t.Errorf("staging/stage3 file not found")
+    }
+}
 
 // TODO 
 func TestStartHypervisor(t *testing.T) {
