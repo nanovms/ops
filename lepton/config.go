8e452c09f915795d6481ec5476c1fe71eca035a1
externalreality <ecjones@protonmail.com>
2019-01-23 15:08:51 -0500

    Separate flags for nightly and updates.

--- lepton/config.go
+++ lepton/config.go
@@ -17,6 +17,7 @@ type Config struct {
 	NameServer   string
 	NightlyBuild bool
 	RunConfig    RunConfig
+	Force        bool
 }
 
 // Runtime configs
--- lepton/package.go
+++ lepton/package.go
@@ -83,9 +83,9 @@ func BuildImageFromPackage(packagepath string, c Config) error {
 func BuildFromPackage(packagepath string, c *Config) error {
 	var err error
 	if c.NightlyBuild {
-		err = DownloadImages(DevBaseUrl, c.NightlyBuild)
+		err = DownloadImages(DevBaseUrl, c.Force)
 	} else {
-		err = DownloadImages(ReleaseBaseUrl, c.NightlyBuild)
+		err = DownloadImages(ReleaseBaseUrl, c.Force)
 	}
 	if err != nil {
 		return err
