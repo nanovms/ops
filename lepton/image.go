4817b40d931224af2c1b38cb0a5744086875ffa8
tijoy <tijoy@nanovms.com>
2019-02-20 11:23:07 -0800

    clean up tempimage

--- lepton/image.go
+++ lepton/image.go
@@ -227,7 +227,12 @@ func buildImage(c *Config, m *Manifest) error {
 
 	// produce final image, boot + kernel + elf
 	fd, err := createFile(c.DiskImage)
-	defer fd.Close()
+
+	defer func() {
+		os.Remove(mergedImg)
+		fd.Close()
+	}()
+
 	if err != nil {
 		return errors.Wrap(err, 1)
 	}
