889d0e58fb3dc301b542f1e57a2d68fad44c0528
Sanket Sudake <sanket@infracloud.io>
2019-07-01 12:00:04 +0530

    Create logs per e2e test

--- e2e/run_e2e.sh
+++ e2e/run_e2e.sh
@@ -5,14 +5,15 @@ ruby_2.5.1
 nginx_1.15.6
 node_v11.5.0"
 
-if [ -f /tmp/e2erun.log ] ; then
-    rm /tmp/e2erun.log
+if [ -d /tmp/e2erun ] ; then
+    rm -r /tmp/e2erun
 fi
+mkdir /tmp/e2erun
 
 for test in $TESTS
 do
     cd $test
-    ./test.sh >>/tmp/e2erun.log 2>&1
+    ./test.sh >>/tmp/e2erun/$test.log 2>&1
     if [ $? -eq 0 ] ; then
         echo "$test: PASSED"
     else
