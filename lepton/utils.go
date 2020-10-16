bcb62609436f473e5bfd6ebe2e92151726000738
Chaitanya Baraskar <chaitanya.axe@gmail.com>
2020-10-16 18:14:39 +0530

    Expose list of ports #705 (#709)
    * Issue #705
    
    -Added support to expose list of ports given by user.
    -Removed http and https ports as default ports.
    -In case user doesn't provide flavour, added default flavour "g1-small"
    
    * -Added separate function to convert int array to string.
    -Changed firewall rule naming convention

--- lepton/utils.go
+++ lepton/utils.go
@@ -3,9 +3,14 @@ package lepton
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func exitWithError(errs string) {
 	fmt.Println(fmt.Sprintf(ErrorColor, errs))
 	os.Exit(1)
+}
+
+func arrayToString(a []int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
