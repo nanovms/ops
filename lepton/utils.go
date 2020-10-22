d9186836b52f706d4ed6e2f4ca9ae56b3e6f567c
Fábio Ferreira <martinhoferreira10@gmail.com>
2020-10-22 17:57:39 +0100

    Reuse code of dns record creation.
    * Add dns service interface methods to gcp and aws.
    
    * Use CreateDNSRecord global function to create records in multiple cloud providers.
    
    * Throw error in isDomainValid if domain is empty.

--- lepton/utils.go
+++ lepton/utils.go
@@ -23,9 +23,9 @@ func arrayToString(a []int, delim string) string {
 func isDomainValid(name string) error {
 	switch {
 	case len(name) == 0:
-		return nil // an empty domain name will result in a cookie without a domain restriction
+		return fmt.Errorf("domain: empty")
 	case len(name) > 255:
-		return fmt.Errorf("cookie domain: name length is %d, can't exceed 255", len(name))
+		return fmt.Errorf("domain: name length is %d, can't exceed 255", len(name))
 	}
 	var l int
 	for i := 0; i < len(name); i++ {
@@ -34,13 +34,13 @@ func isDomainValid(name string) error {
 			// check domain labels validity
 			switch {
 			case i == l:
-				return fmt.Errorf("cookie domain: invalid character '%c' at offset %d: label can't begin with a period", b, i)
+				return fmt.Errorf("domain: invalid character '%c' at offset %d: label can't begin with a period", b, i)
 			case i-l > 63:
-				return fmt.Errorf("cookie domain: byte length of label '%s' is %d, can't exceed 63", name[l:i], i-l)
+				return fmt.Errorf("domain: byte length of label '%s' is %d, can't exceed 63", name[l:i], i-l)
 			case name[l] == '-':
-				return fmt.Errorf("cookie domain: label '%s' at offset %d begins with a hyphen", name[l:i], l)
+				return fmt.Errorf("domain: label '%s' at offset %d begins with a hyphen", name[l:i], l)
 			case name[i-1] == '-':
-				return fmt.Errorf("cookie domain: label '%s' at offset %d ends with a hyphen", name[l:i], l)
+				return fmt.Errorf("domain: label '%s' at offset %d ends with a hyphen", name[l:i], l)
 			}
 			l = i + 1
 			continue
@@ -50,23 +50,23 @@ func isDomainValid(name string) error {
 			// show the printable unicode character starting at byte offset i
 			c, _ := utf8.DecodeRuneInString(name[i:])
 			if c == utf8.RuneError {
-				return fmt.Errorf("cookie domain: invalid rune at offset %d", i)
+				return fmt.Errorf("domain: invalid rune at offset %d", i)
 			}
-			return fmt.Errorf("cookie domain: invalid character '%c' at offset %d", c, i)
+			return fmt.Errorf("domain: invalid character '%c' at offset %d", c, i)
 		}
 	}
 	// check top level domain validity
 	switch {
 	case l == len(name):
-		return fmt.Errorf("cookie domain: missing top level domain, domain can't end with a period")
+		return fmt.Errorf("domain: missing top level domain, domain can't end with a period")
 	case len(name)-l > 63:
-		return fmt.Errorf("cookie domain: byte length of top level domain '%s' is %d, can't exceed 63", name[l:], len(name)-l)
+		return fmt.Errorf("domain: byte length of top level domain '%s' is %d, can't exceed 63", name[l:], len(name)-l)
 	case name[l] == '-':
-		return fmt.Errorf("cookie domain: top level domain '%s' at offset %d begins with a hyphen", name[l:], l)
+		return fmt.Errorf("domain: top level domain '%s' at offset %d begins with a hyphen", name[l:], l)
 	case name[len(name)-1] == '-':
-		return fmt.Errorf("cookie domain: top level domain '%s' at offset %d ends with a hyphen", name[l:], l)
+		return fmt.Errorf("domain: top level domain '%s' at offset %d ends with a hyphen", name[l:], l)
 	case name[l] >= '0' && name[l] <= '9':
-		return fmt.Errorf("cookie domain: top level domain '%s' at offset %d begins with a digit", name[l:], l)
+		return fmt.Errorf("domain: top level domain '%s' at offset %d begins with a digit", name[l:], l)
 	}
 	return nil
 }
