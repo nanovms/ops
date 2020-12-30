0c1a9f94ef7cbdd768092704bbf92adb7353ed00
Fredrik Bakken <fredda10x@gmail.com>
2020-12-30 11:21:46 +0100

    Reorder config and descriptions initiated.

--- lepton/config.go
+++ lepton/config.go
@@ -2,40 +2,94 @@ package lepton
 
 // Config for Build
 type Config struct {
-	Args         []string
-	BuildDir     string
-	Dirs         []string
-	Files        []string
-	MapDirs      map[string]string
-	Env          map[string]string
-	Debugflags   []string
-	NoTrace      []string
-	Program      string
-	ProgramPath  string // original path of the program to refer to on attach/detach
-	Version      string
-	Boot         string
-	Kernel       string
-	Mkfs         string
-	NameServer   string
+	// Args defines an array of commands to execute when the image is launched.
+	Args []string
+
+	// BaseVolumeSz is an optional parameter for defining the size of the base
+	// volume (defaults to the end of blocks written by TFS).
+	BaseVolumeSz string
+
+	// Boot - To be described...
+	Boot string
+
+	// BuildDir - To be described...
+	BuildDir string
+
+	// CloudConfig - To be described...
+	CloudConfig ProviderConfig
+
+	// Debugflags - To be described...
+	Debugflags []string
+
+	// Dirs defines an array of directory locations to include into the image.
+	Dirs []string
+
+	// Env defines a map of environment variables to specify for the image
+	// runtime.
+	Env map[string]string
+
+	// Files defines an array of file locations to include into the image.
+	Files []string
+
+	// Force - To be described...
+	Force bool
+
+	// Kernel - To be described...
+	Kernel string
+
+	// ManifestName defines the name of the manifest file.
+	ManifestName string
+
+	// MapDirs specifies a map of local directories to add to into the image.
+	// These directory paths are then adjusted from local path specification
+	// to image path specification.
+	MapDirs map[string]string
+
+	// Mkfs - To be described...
+	Mkfs string
+
+	// Mounts - To be described...
+	Mounts map[string]string
+
+	// NameServer is an optional parameter that defines the DNS server to use
+	// for DNS resolutions (defaults to Google's DNS server: '8.8.8.8').
+	NameServer string
+
+	// NightlyBuild - To be described...
 	NightlyBuild bool
-	RunConfig    RunConfig
-	CloudConfig  ProviderConfig
-	Force        bool
-	TargetRoot   string
-	BaseVolumeSz string // optional base volume sz
-	ManifestName string // save manifest to
-	RebootOnExit bool   // Reboot on Failure Exit
-	Mounts       map[string]string
+
+	// NoTrace - To be described...
+	NoTrace []string
+
+	// Program - To be described...
+	Program string
+
+	// ProgramPath specifies the original path of the program to refer to on
+	// attach/detach.
+	ProgramPath string
+
+	// RebootOnExit defines whether the image should automatically reboot
+	// if an error/failure occurs.
+	RebootOnExit bool
+
+	// RunConfig - To be described...
+	RunConfig RunConfig
+
+	// TargetRoot - To be described...
+	TargetRoot string
+
+	// Version - To be described...
+	Version string
 }
 
 // ProviderConfig give provider details
 type ProviderConfig struct {
+	BucketName string `cloud:"bucketname"`
+	Flavor     string `cloud:"flavor"`
+	ImageName  string `cloud:"imagename"`
 	Platform   string `cloud:"platform"`
 	ProjectID  string `cloud:"projectid"`
 	Zone       string `cloud:"zone"`
-	BucketName string `cloud:"bucketname"`
-	ImageName  string `cloud:"imagename"`
-	Flavor     string `cloud:"flavor"`
 }
 
 // Tag is used as property on creating instances
@@ -46,35 +100,35 @@ type Tag struct {
 
 // RunConfig provides runtime details
 type RunConfig struct {
-	Imagename      string // FIXME: fullpath? of image
-	BaseName       string // FIXME: basename of image only
-	Ports          []string
-	DomainName     string
-	GdbPort        int
-	CPUs           int // number of cpus
-	Verbose        bool
-	Memory         string
-	Bridged        bool
-	TapName        string
-	IPAddr         string
-	Gateway        string
-	NetMask        string
 	Accel          bool
-	UDP            bool // enable UDP
-	UDPPorts       []string
-	OnPrem         bool // true if in a multi-instance/tenant on-prem env
+	BaseName       string // FIXME: basename of image only
+	Bridged        bool
+	CPUs           int // number of cpus
+	Debug          bool
+	DomainName     string
+	Gateway        string
+	GdbPort        int
+	Imagename      string // FIXME: fullpath? of image
+	InstanceName   string
+	IPAddr         string
+	Klibs          []string
+	Memory         string
 	Mounts         []string
-	VolumeSizeInGb int //This option is only for openstack.
-	VPC            string
+	NetMask        string
+	OnPrem         bool // true if in a multi-instance/tenant on-prem env
+	Ports          []string
 	SecurityGroup  string
+	ShowDebug      bool
+	ShowErrors     bool
+	ShowWarnings   bool
 	Subnet         string
 	Tags           []Tag
-	Debug          bool
-	ShowWarnings   bool
-	ShowErrors     bool
-	ShowDebug      bool
-	Klibs          []string
-	InstanceName   string
+	TapName        string
+	UDP            bool // enable UDP
+	UDPPorts       []string
+	Verbose        bool
+	VolumeSizeInGb int //This option is only for openstack.
+	VPC            string
 }
 
 // RuntimeConfig constructs runtime config
