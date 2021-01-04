b73f880d7edd248875bbd3664fc81557e515cc76
Fábio Ferreira <martinhoferreira10@gmail.com>
2021-01-04 17:05:33 +0000

    Use OS commands to configure bridge and tap device (#836)

--- cmd/run.go
+++ cmd/run.go
@@ -12,6 +12,7 @@ import (
 	"github.com/go-errors/errors"
 
 	api "github.com/nanovms/ops/lepton"
+	"github.com/nanovms/ops/network"
 	"github.com/spf13/cobra"
 )
 
@@ -273,8 +274,140 @@ func runCommandHandler(cmd *cobra.Command, args []string) {
 
 	fmt.Printf("booting %s ...\n", c.RunConfig.Imagename)
 
+	if tapDeviceName != "" {
+		tapExists, err := network.CheckNetworkInterfaceExists(tapDeviceName)
+		if err != nil {
+			panic("Not able to check tap exists")
+		}
+
+		if !tapExists {
+			_, err := network.AddTap(tapDeviceName)
+			if err != nil {
+				panic("Not able to create tap")
+			}
+		}
+
+		bridgeName := "br0"
+
+		if bridged {
+
+			bridgeExists, err := network.CheckNetworkInterfaceExists(bridgeName)
+			if err != nil {
+				panic("Not able to check if bridge exists")
+			}
+
+			if !bridgeExists {
+				_, err := network.AddBridge(bridgeName)
+				if err != nil {
+					panic("Not able to create bridge")
+				}
+			}
+
+			if ipaddr != "" {
+				ip := net.ParseIP(ipaddr).To4()
+				ip[3] = byte(66) // 66 is a random IP
+				bridgeIP := ip.String()
+
+				currentBridgeIP, err := network.GetNetworkInterfaceIP(bridgeName)
+				if err != nil {
+					panic("Not able to get current bridge IP")
+				}
+
+				if currentBridgeIP != bridgeIP {
+					_, err = network.FlushIPFromNI(bridgeName)
+					if err != nil {
+						panic("Not able to flush bridge IPs")
+					}
+
+					_, err = network.SetNIIP(bridgeName, bridgeIP, netmask)
+					if err != nil {
+						panic("Not able to assign IP to bridge")
+					}
+				}
+
+			}
+
+			isTapInTheBridge, err := network.CheckBridgeHasInterface(bridgeName, tapDeviceName)
+			if err != nil {
+				panic("Not able to check if tap is in the bridge")
+			}
+
+			if !isTapInTheBridge {
+				_, err := network.AddTapToBridge(bridgeName, tapDeviceName)
+				if err != nil {
+					panic("Not able to add tap to bridge")
+				}
+			}
+
+			isBridgeUp, err := network.IsNIUp(bridgeName)
+			if err != nil {
+				panic("Not able to check if bridge is up")
+			}
+
+			if !isBridgeUp {
+				_, err := network.TurnNIUp(bridgeName)
+				if err != nil {
+					panic("Not able to turn bridge up")
+				}
+			}
+		}
+
+		isTapUp, err := network.IsNIUp(tapDeviceName)
+		if err != nil {
+			panic("Not able to check if tap is up")
+		}
+
+		if !isTapUp {
+			_, err = network.TurnNIUp(tapDeviceName)
+			if err != nil {
+				panic("Not able to turn tap up")
+			}
+		}
+	}
+
 	initDefaultRunConfigs(c, ports)
 	hypervisor.Start(&c.RunConfig)
+
+	if tapDeviceName != "" {
+		_, err := network.TurnNIDown(tapDeviceName)
+		if err != nil {
+			panic("Not able to turn tap down")
+		}
+
+		if bridged {
+			bridgeInterfaces, err := network.GetBridgeInterfacesNames(bridgeName)
+			if err != nil {
+				panic("Not able to get network interfaces in bridge")
+			}
+
+			var someBridgeInterfaceIsUp bool
+
+			for _, bi := range bridgeInterfaces {
+				isUp, err := network.IsNIUp(bi)
+				if err != nil {
+					panic("Not able to get interface state")
+				}
+
+				if isUp {
+					someBridgeInterfaceIsUp = true
+					break
+				}
+			}
+
+			if !someBridgeInterfaceIsUp {
+				_, err := network.TurnNIDown(bridgeName)
+				if err != nil {
+					panic("Not able to turn bridge down")
+				}
+
+				_, err = network.FlushIPFromNI(bridgeName)
+				if err != nil {
+					panic("Not able to flush IPs from bridge")
+				}
+			}
+
+		}
+	}
 }
 
 // RunCommand provides support for running binary with nanos
