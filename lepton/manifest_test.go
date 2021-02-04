fb60e61706f3be7c2376164ab9381ccd119a9ad0
Fábio Ferreira <martinhoferreira10@gmail.com>
2021-02-04 10:01:14 +0000

    add ntp poll limits manifest variables if specified as environment variables (closes #885)

--- lepton/manifest_test.go
+++ lepton/manifest_test.go
@@ -5,6 +5,8 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -117,4 +119,103 @@ func TestAddRadarEnvAddKlibs(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
 	}
+}
+
+func TestAddNTPEnvVarsToManifestFile(t *testing.T) {
+
+	t.Run("should not add ntp manifest variables if ntp klib is not specified", func(t *testing.T) {
+		m := NewManifest("")
+		m.AddEnvironmentVariable("ntpAddress", "127.0.0.1")
+		m.AddEnvironmentVariable("ntpPort", "1234")
+		m.AddEnvironmentVariable("ntpPollMin", "5")
+		m.AddEnvironmentVariable("ntpPollMax", "10")
+
+		manifestFile := m.String()
+
+		assert.NotContains(t, manifestFile, "ntp_address:127.0.0.1\n")
+		assert.NotContains(t, manifestFile, "ntp_port:1234\n")
+		assert.NotContains(t, manifestFile, "ntp_poll_min:5\n")
+		assert.NotContains(t, manifestFile, "ntp_poll_max:10\n")
+	})
+
+	t.Run("should add ntp manifest variables if ntp klib is added and environment variables are valid", func(t *testing.T) {
+		m := NewManifest("")
+		m.AddKlibs([]string{"ntp"})
+		m.AddEnvironmentVariable("ntpAddress", "127.0.0.1")
+		m.AddEnvironmentVariable("ntpPort", "1234")
+		m.AddEnvironmentVariable("ntpPollMin", "5")
+		m.AddEnvironmentVariable("ntpPollMax", "10")
+
+		manifestFile := m.String()
+
+		assert.Contains(t, manifestFile, "ntp_address:127.0.0.1\n")
+		assert.Contains(t, manifestFile, "ntp_port:1234\n")
+		assert.Contains(t, manifestFile, "ntp_poll_min:5\n")
+		assert.Contains(t, manifestFile, "ntp_poll_max:10\n")
+	})
+
+	t.Run("should not ntp poll limits if min is greater than max", func(t *testing.T) {
+		m := NewManifest("")
+		m.AddKlibs([]string{"ntp"})
+		m.AddEnvironmentVariable("ntpPollMin", "10")
+		m.AddEnvironmentVariable("ntpPollMax", "5")
+
+		manifestFile := m.String()
+
+		assert.NotContains(t, manifestFile, "ntp_poll_min:10\n")
+		assert.NotContains(t, manifestFile, "ntp_poll_max:5\n")
+	})
+
+	t.Run("should not add ntp poll min value if is lower than 4", func(t *testing.T) {
+		m := NewManifest("")
+		m.AddKlibs([]string{"ntp"})
+		m.AddEnvironmentVariable("ntpPollMin", "3")
+
+		manifestFile := m.String()
+
+		assert.NotContains(t, manifestFile, "ntp_poll_min:5\n")
+	})
+
+	t.Run("should add ntp poll min value if is greater than 3", func(t *testing.T) {
+		m := NewManifest("")
+		m.AddKlibs([]string{"ntp"})
+		m.AddEnvironmentVariable("ntpPollMin", "5")
+
+		manifestFile := m.String()
+
+		assert.Contains(t, manifestFile, "ntp_poll_min:5\n")
+	})
+
+	t.Run("should not add ntp poll max value if is greater than 17", func(t *testing.T) {
+		m := NewManifest("")
+		m.AddKlibs([]string{"ntp"})
+		m.AddEnvironmentVariable("ntpPollMax", "18")
+
+		manifestFile := m.String()
+
+		assert.NotContains(t, manifestFile, "ntp_poll_max:18\n")
+	})
+
+	t.Run("should add ntp poll max value if is lower than 18", func(t *testing.T) {
+		m := NewManifest("")
+		m.AddKlibs([]string{"ntp"})
+		m.AddEnvironmentVariable("ntpPollMax", "17")
+
+		manifestFile := m.String()
+
+		assert.Contains(t, manifestFile, "ntp_poll_max:17\n")
+	})
+
+	t.Run("should not add ntp poll max/min values if they are not numbers", func(t *testing.T) {
+		m := NewManifest("")
+		m.AddKlibs([]string{"ntp"})
+		m.AddEnvironmentVariable("ntpPollMin", "asd")
+		m.AddEnvironmentVariable("ntpPollMax", "dsa")
+
+		manifestFile := m.String()
+
+		assert.NotContains(t, manifestFile, "ntp_poll_min:asd\n")
+		assert.NotContains(t, manifestFile, "ntp_poll_max:dsa\n")
+	})
+
 }
