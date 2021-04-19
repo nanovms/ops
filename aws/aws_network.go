8d2886f8ca0c6dd8e41df0032e614f6605939e7d
Fábio Ferreira <martinhoferreira10@gmail.com>
2021-04-19 06:39:26 +0100

    support either ids and names for aws vpc/subnet/sg in configuration (closes #1002)

--- aws/aws_network.go
+++ aws/aws_network.go
@@ -15,61 +15,97 @@ import (
 	"github.com/nanovms/ops/types"
 )
 
-// CheckValidSecurityGroup checks whether the configuration security group exists and has the configuration VPC assigned
-func (p *AWS) CheckValidSecurityGroup(ctx *lepton.Context, svc *ec2.EC2) error {
-	sg := ctx.Config().CloudConfig.SecurityGroup
-	vpc := ctx.Config().CloudConfig.VPC
+// GetSecurityGroup checks whether the configuration security group exists and has the configuration VPC assigned
+func (p *AWS) GetSecurityGroup(ctx *lepton.Context, svc *ec2.EC2, vpc *ec2.Vpc) (sg *ec2.SecurityGroup, err error) {
+	sgName := ctx.Config().CloudConfig.SecurityGroup
 
 	input := &ec2.DescribeSecurityGroupsInput{
-		GroupIds: []*string{
-			aws.String(sg),
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("group-name"),
+				Values: aws.StringSlice([]string{sgName}),
+			},
 		},
 	}
 
-	result, err := svc.DescribeSecurityGroups(input)
+	var result *ec2.DescribeSecurityGroupsOutput
+
+	result, err = svc.DescribeSecurityGroups(input)
 	if err != nil {
-		return fmt.Errorf("get security group with id '%s': %s", sg, err.Error())
-	}
-
-	if len(result.SecurityGroups) == 1 && *result.SecurityGroups[0].VpcId != vpc {
-		return fmt.Errorf("vpc mismatch: expected '%s' to have vpc '%s', got '%s'", sg, ctx.Config().CloudConfig.VPC, *result.SecurityGroups[0].VpcId)
+		return
 	} else if len(result.SecurityGroups) == 0 {
-		return fmt.Errorf("security group '%s' not found", sg)
+		input := &ec2.DescribeSecurityGroupsInput{
+			GroupIds: []*string{
+				aws.String(sgName),
+			},
+		}
+
+		result, err = svc.DescribeSecurityGroups(input)
+		if err != nil {
+			err = fmt.Errorf("get security group with id '%s': %s", sg, err.Error())
+			return
+		}
 	}
 
-	return nil
+	if len(result.SecurityGroups) == 1 && *result.SecurityGroups[0].VpcId != *vpc.VpcId {
+		err = fmt.Errorf("vpc mismatch: expected '%s' to have vpc '%s', got '%s'", sgName, *vpc.VpcId, *result.SecurityGroups[0].VpcId)
+		return
+	} else if len(result.SecurityGroups) == 0 {
+		err = fmt.Errorf("security group '%s' not found", sgName)
+		return
+	}
+
+	sg = result.SecurityGroups[0]
+
+	return
 }
 
 // GetSubnet returns a subnet with the context subnet name or the default subnet of vpc passed by argument
 func (p *AWS) GetSubnet(ctx *lepton.Context, svc *ec2.EC2, vpcID string) (*ec2.Subnet, error) {
 	subnetName := ctx.Config().CloudConfig.Subnet
 	var filters []*ec2.Filter
+	var result *ec2.DescribeSubnetsOutput
+	var err error
 
 	filters = append(filters, &ec2.Filter{Name: aws.String("vpc-id"), Values: aws.StringSlice([]string{vpcID})})
 
 	if subnetName != "" {
-		filters = append(filters, &ec2.Filter{Name: aws.String("tag:Name"), Values: aws.StringSlice([]string{ctx.Config().CloudConfig.Subnet})})
-	}
+		result, err = svc.DescribeSubnets(&ec2.DescribeSubnetsInput{
+			Filters: append(filters, &ec2.Filter{Name: aws.String("tag:Name"), Values: aws.StringSlice([]string{subnetName})}),
+		})
+		if err != nil {
+			err = fmt.Errorf("unable to describe subnets, %v", err)
+			return nil, err
+		} else if len(result.Subnets) == 0 {
+			result, err = svc.DescribeSubnets(&ec2.DescribeSubnetsInput{
+				SubnetIds: aws.StringSlice([]string{subnetName}),
+				Filters:   filters,
+			})
+			if err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		input := &ec2.DescribeSubnetsInput{
+			Filters: filters,
+		}
 
-	input := &ec2.DescribeSubnetsInput{
-		Filters: filters,
-	}
-
-	result, err := svc.DescribeSubnets(input)
-	if err != nil {
-		fmt.Printf("Unable to describe subnets, %v\n", err)
-		return nil, err
+		result, err = svc.DescribeSubnets(input)
+		if err != nil {
+			err = fmt.Errorf("unable to describe subnets, %v", err)
+			return nil, err
+		}
 	}
 
 	if len(result.Subnets) == 0 && subnetName != "" {
-		return nil, fmt.Errorf("No Subnets with name '%v' found to associate security group with", subnetName)
+		return nil, fmt.Errorf("no subnets with name '%v' found to associate security group with", subnetName)
 	} else if len(result.Subnets) == 0 {
-		return nil, errors.New("No Subnets found to associate security group with")
+		return nil, errors.New("no subnets found to associate security group with")
 	}
 
 	if subnetName != "" {
 		for _, subnet := range result.Subnets {
-			if *subnet.DefaultForAz == true {
+			if *subnet.DefaultForAz {
 				return subnet, nil
 			}
 		}
@@ -83,23 +119,40 @@ func (p *AWS) GetVPC(ctx *lepton.Context, svc *ec2.EC2) (*ec2.Vpc, error) {
 	vpcName := ctx.Config().CloudConfig.VPC
 	var vpc *ec2.Vpc
 	var input *ec2.DescribeVpcsInput
+	var result *ec2.DescribeVpcsOutput
+	var err error
+
 	if vpcName != "" {
 		var filters []*ec2.Filter
 
-		filters = append(filters, &ec2.Filter{Name: aws.String("tag:Name"), Values: aws.StringSlice([]string{ctx.Config().CloudConfig.VPC})})
+		filters = append(filters, &ec2.Filter{Name: aws.String("tag:Name"), Values: aws.StringSlice([]string{vpcName})})
 		input = &ec2.DescribeVpcsInput{
 			Filters: filters,
 		}
+
+		result, err = svc.DescribeVpcs(input)
+		if err != nil {
+			return nil, fmt.Errorf("unable to describe VPCs, %v", err)
+		} else if len(result.Vpcs) == 0 {
+			input = &ec2.DescribeVpcsInput{
+				VpcIds: aws.StringSlice([]string{vpcName}),
+			}
+			result, err = svc.DescribeVpcs(input)
+			if err != nil {
+				return nil, fmt.Errorf("unable to describe VPCs, %v", err)
+			}
+		}
+	} else {
+		result, err = svc.DescribeVpcs(input)
+		if err != nil {
+			return nil, fmt.Errorf("unable to describe VPCs, %v", err)
+		}
 	}
 
-	result, err := svc.DescribeVpcs(input)
-	if err != nil {
-		return nil, fmt.Errorf("Unable to describe VPCs, %v", err)
-	}
 	if len(result.Vpcs) == 0 && vpcName != "" {
 		return nil, nil
 	} else if len(result.Vpcs) == 0 {
-		return nil, errors.New("No VPCs found to associate security group with")
+		return nil, errors.New("no VPCs found to associate security group with")
 	}
 
 	if vpcName != "" {
@@ -108,7 +161,7 @@ func (p *AWS) GetVPC(ctx *lepton.Context, svc *ec2.EC2) (*ec2.Vpc, error) {
 		// select default vpc
 		for i, s := range result.Vpcs {
 			isDefault := *s.IsDefault
-			if isDefault == true {
+			if isDefault {
 				vpc = result.Vpcs[i]
 			}
 		}
@@ -154,7 +207,7 @@ func (p AWS) buildFirewallRule(protocol string, port string) *ec2.IpPermission {
 }
 
 // CreateSG - Create security group
-func (p *AWS) CreateSG(ctx *lepton.Context, svc *ec2.EC2, imgName string, vpcID string) (string, error) {
+func (p *AWS) CreateSG(ctx *lepton.Context, svc *ec2.EC2, imgName string, vpcID string) (sg *ec2.SecurityGroup, err error) {
 	t := time.Now().UnixNano()
 	s := strconv.FormatInt(t, 10)
 
@@ -170,15 +223,17 @@ func (p *AWS) CreateSG(ctx *lepton.Context, svc *ec2.EC2, imgName string, vpcID 
 			switch aerr.Code() {
 			case "InvalidVpcID.NotFound":
 				errstr := fmt.Sprintf("Unable to find VPC with ID %q.", vpcID)
-				return "", errors.New(errstr)
+				err = errors.New(errstr)
+				return
 			case "InvalidGroup.Duplicate":
 				errstr := fmt.Sprintf("Security group %q already exists.", imgName)
-				return "", errors.New(errstr)
+				err = errors.New(errstr)
+				return
 			}
 		}
 		errstr := fmt.Sprintf("Unable to create security group %q, %v", imgName, err)
-		return "", errors.New(errstr)
-
+		err = errors.New(errstr)
+		return
 	}
 	fmt.Printf("Created security group %s with VPC %s.\n",
 		aws.StringValue(createRes.GroupId), vpcID)
@@ -203,11 +258,23 @@ func (p *AWS) CreateSG(ctx *lepton.Context, svc *ec2.EC2, imgName string, vpcID 
 		})
 		if err != nil {
 			errstr := fmt.Sprintf("Unable to set security group %q ingress, %v", imgName, err)
-			return "", errors.New(errstr)
+			err = errors.New(errstr)
+			return
 		}
 	}
 
-	return aws.StringValue(createRes.GroupId), nil
+	result, err := svc.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
+		GroupIds: aws.StringSlice([]string{*createRes.GroupId}),
+	})
+	if err != nil {
+		return
+	} else if len(result.SecurityGroups) == 0 {
+		err = errors.New("failed creating security group")
+	}
+
+	sg = result.SecurityGroups[0]
+
+	return
 }
 
 // CreateVPC creates a virtual network
