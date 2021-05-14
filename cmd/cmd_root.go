70a813bb5130c1f9011f2d055f004716c5e21bf3
Lufty <lufty.abdillah@gmail.com>
2021-05-14 21:10:53 +0700

    Always check global flags

--- cmd/cmd_root.go
+++ cmd/cmd_root.go
@@ -14,42 +14,20 @@ func GetRootCommand() *cobra.Command {
 	var rootCmd = &cobra.Command{
 		Use: "ops",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			var config *types.Config
+			config := &types.Config{}
 
 			configFlag, _ := cmd.Flags().GetString("config")
 			configFlag = strings.TrimSpace(configFlag)
 
 			if configFlag != "" {
-				config := &types.Config{}
 				if err := unWarpConfig(configFlag, config); err != nil {
 					return err
 				}
-
-				globalFlags := NewGlobalCommandFlags(cmd.Flags())
-				if err := globalFlags.MergeToConfig(config); err != nil {
-					return err
-				}
 			}
 
-			showErrors, err := cmd.Flags().GetBool("show-errors")
-			if err != nil {
+			globalFlags := NewGlobalCommandFlags(cmd.Flags())
+			if err := globalFlags.MergeToConfig(config); err != nil {
 				return err
-			}
-			showDebug, err := cmd.Flags().GetBool("show-debug")
-			if err != nil {
-				return err
-			}
-			showWarnings, err := cmd.Flags().GetBool("show-warnings")
-			if err != nil {
-				return err
-			}
-			if showErrors || showDebug || showWarnings {
-				if config == nil {
-					config = &types.Config{}
-				}
-				config.RunConfig.ShowErrors = showErrors
-				config.RunConfig.ShowDebug = showDebug
-				config.RunConfig.ShowWarnings = showWarnings
 			}
 
 			log.InitDefault(os.Stdout, config)
