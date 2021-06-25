f007168c6732c0e00c7f32f8b2240b0477c002fa
Nathanael Demacon <nathanael.dmc@outlook.fr>
2021-06-25 17:40:53 +0200

    Optimize `HomeDir()` (#1095) (#1097)

--- lepton/homedir.go
+++ lepton/homedir.go
@@ -5,18 +5,26 @@ import (
 	"os"
 )
 
+var homeDir = ""
+
 // HomeDir returns the home directory for the executing user.
 // This uses an OS-specific method for discovering the home directory.
 // An error is returned if a home directory cannot be detected.
 func HomeDir() (string, error) {
-	if home, err := os.UserHomeDir(); err == nil {
-		return home, nil
+	if homeDir != "" {
+		return homeDir, nil
 	}
 
-	path, err := os.Getwd()
+	var err error = nil
+
+	if homeDir, err = os.UserHomeDir(); err == nil {
+		return homeDir, nil
+	}
+
+	homeDir, err = os.Getwd()
 	if err != nil {
 		return "", errors.New("home directory not detected")
 	}
 
-	return path, nil
+	return homeDir, nil
 }
