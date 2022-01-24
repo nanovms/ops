c848687d829a4d8cabf655271fac2bb70df9deb1
Viktor Patchev <viktor.patchev@inplayer.com>
2022-01-24 11:02:57 +0100

    Fmt

--- cmd/flags_config.go
+++ cmd/flags_config.go
@@ -65,7 +65,7 @@ func ConvertJSONToConfig(data []byte, c *types.Config) (err error) {
 	err = dec.Decode(&c)
 	if err != nil {
 		if jsonErr, ok := err.(*json.SyntaxError); ok {
-			problemPart := data[jsonErr.Offset-1: jsonErr.Offset+10]
+			problemPart := data[jsonErr.Offset-1 : jsonErr.Offset+10]
 			line := 1 + strings.Count(string(data)[:jsonErr.Offset], "\n")
 			err = fmt.Errorf("%w ~ error near '%s' (offset %d) line: %v", err, problemPart, jsonErr.Offset, line)
 		}
