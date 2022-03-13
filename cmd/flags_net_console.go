d280d99d60c790826579046ecb9f19911f4997dd
girish-nanovms <98557274+girish-nanovms@users.noreply.github.com>
2022-03-13 23:46:08 +0200

    use string port to fix panic

--- cmd/flags_net_console.go
+++ cmd/flags_net_console.go
@@ -7,7 +7,7 @@ import (
 
 // NetConsoleFlags for flags related to net console options
 type NetConsoleFlags struct {
-	NetConsolePort int
+	NetConsolePort string
 	NetConsoleIP   string
 	Consoles       []string
 }
@@ -17,7 +17,7 @@ func NewNetConsoleFlags(cmdFlags *pflag.FlagSet) (flags *NetConsoleFlags) {
 	var err error
 	flags = &NetConsoleFlags{}
 
-	flags.NetConsolePort, err = cmdFlags.GetInt("netconsole-port")
+	flags.NetConsolePort, err = cmdFlags.GetString("netconsole-port")
 	if err != nil {
 		exitWithError(err.Error())
 	}
@@ -46,7 +46,7 @@ func (flags *NetConsoleFlags) MergeToConfig(config *types.Config) (err error) {
 
 // PersistNetConsoleFlags append flag to a command
 func PersistNetConsoleFlags(cmdFlags *pflag.FlagSet) {
-	cmdFlags.IntP("netconsole-port", "", 4444, "set net console port")
+	cmdFlags.StringP("netconsole-port", "", "4444", "set net console port")
 	cmdFlags.StringP("netconsole-ip", "", "10.0.2.2", "set net console ip")
 	cmdFlags.StringArrayP("consoles", "", []string{}, "set different consoles to forward logs to")
 }
