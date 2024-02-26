5a1ed704451c81b771dca63c2747dba1bfd6d3b5
Henrik Nordgren <77230891+hnord-vdx@users.noreply.github.com>
2024-02-26 19:34:45 +0100

    Added GetVPC method
    Fetches all VPCs no matter what and returns last matching VPC (the first VPC created using `vpcName`)

---
+++ provider/digitalocean/digital_ocean_network.go
@@ -0,0 +1,56 @@
+//go:build digitalocean || do || !onlyprovider
+
+package digitalocean
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/digitalocean/godo"
+	"github.com/nanovms/ops/lepton"
+)
+
+// ListVpcs - List all VPCs
+func (do *DigitalOcean) GetVPC(ctx *lepton.Context, vpcName string) (*godo.VPC, error) {
+
+	if vpcName == "" {
+		return nil, nil
+	}
+
+	page := 1
+	var vpc *godo.VPC
+
+	// ctx.Logger().Debug("getting all vpcs")
+
+	for {
+		opts := &godo.ListOptions{
+			Page:    page,
+			PerPage: 200, // max allowed by DO
+		}
+
+		vpcs, _, err := do.Client.VPCs.List(context.TODO(), opts)
+		if err != nil {
+			return nil, err
+		}
+		if len(vpcs) == 0 {
+			break
+		}
+
+		for _, v := range vpcs {
+			if v.Name == vpcName {
+				if vpc != nil {
+					ctx.Logger().Debugf("found another vpc %s that matches the criteria %s", v.ID, vpcName)
+				}
+				vpc = v
+			}
+		}
+		page++
+	}
+
+	if vpc == nil {
+		ctx.Logger().Debugf("no vpcs with name %s found", vpcName)
+		return nil, fmt.Errorf("vpc %s not found", vpcName)
+	}
+
+	return vpc, nil
+}
