cf4c950ab1d3ba2010eddb4e70c5513de3b728a9
Vincent Khamsao <vincent.khamsao@gmail.com>
2024-06-11 17:22:21 -0400

    update: move from direct http to linodego client for api requests

--- provider/linode/linode.go
+++ provider/linode/linode.go
@@ -4,10 +4,13 @@ package linode
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/linode/linodego"
 	"github.com/nanovms/ops/lepton"
 	"github.com/nanovms/ops/types"
+	"golang.org/x/oauth2"
 )
 
 // ProviderName of the cloud platform provider
@@ -16,7 +19,7 @@ const ProviderName = "linode"
 // Linode Provider to interact with Linode infrastructure
 type Linode struct {
 	Storage *Objects
-	token   string
+	Client  linodego.Client
 }
 
 // NewProvider Linode
@@ -26,10 +29,20 @@ func NewProvider() *Linode {
 
 // Initialize provider
 func (v *Linode) Initialize(config *types.ProviderConfig) error {
-	v.token = os.Getenv("TOKEN")
-	if v.token == "" {
+	token := os.Getenv("TOKEN")
+	if token == "" {
 		return fmt.Errorf("TOKEN is not set")
 	}
+
+	tokenSource := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+
+	oauth2Client := &http.Client{
+		Transport: &oauth2.Transport{
+			Source: tokenSource,
+		},
+	}
+
+	v.Client = linodego.NewClient(oauth2Client)
 
 	//	ctx := context.Background()
 	return nil
