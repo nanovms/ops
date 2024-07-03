c7dbb48c9331e4e901d3796bdddcf7497a2b1252
Ian Eyberg <ian@nanovms.com>
2024-07-03 02:32:16 +0000

    this adds an arch check for running arm payloads on linux x86

--- qemu/qemu_unix.go
+++ qemu/qemu_unix.go
@@ -455,15 +455,7 @@ func (q *qemu) setConfig(rconfig *types.RunConfig) error {
 		pciBus = "pcie.0"
 	}
 
-	usex86 := true
-	// pcie root ports need to come before virtio/scsi devices
-	if (isx86() && lepton.AltGOARCH == "") || lepton.AltGOARCH == "amd64" {
-		usex86 = true
-	}
-
-	if (!isx86() && lepton.AltGOARCH == "") || lepton.AltGOARCH == "arm64" {
-		usex86 = false
-	}
+	usex86 := archCheck()
 
 	if usex86 {
 		q.addOption("-machine", "q35")
