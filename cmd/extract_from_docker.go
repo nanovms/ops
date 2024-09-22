b894916341fdeb0d02071b31c3e279e843aec329
Ian Eyberg <ian@nanovms.com>
2024-09-22 12:54:19 -0700

    Allow cmd extraction from docker image layers (#1660)
    * if user doesnt specify file of choice try to extract CMD
    
    * reorder CMD extraction in case we dont have the image dld yet

--- cmd/extract_from_docker.go
+++ cmd/extract_from_docker.go
@@ -23,6 +23,52 @@ import (
 	"github.com/nanovms/ops/types"
 )
 
+func getCMDExecutable(imageName string) (string, error) {
+	ctx := context.Background()
+	cli, err := dockerClient.NewClientWithOpts(dockerClient.FromEnv, dockerClient.WithAPIVersionNegotiation())
+	if err != nil {
+		return "", err
+	}
+
+	// grab latest if not specified
+	if !strings.Contains(imageName, ":") {
+		imageName += ":latest"
+	}
+
+	images, err := cli.ImageList(ctx, dockerTypes.ImageListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id := ""
+	for _, img := range images {
+		tags := img.RepoTags
+		for i := 0; i < len(tags); i++ {
+			if tags[i] == imageName {
+				fmt.Printf("found it %s %s\n", img.ID, tags[i])
+				id = img.ID
+				break
+			}
+		}
+	}
+
+	hir, err := cli.ImageHistory(ctx, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	prog := ""
+
+	// could make this a lot smarter in the future
+	if strings.Contains(hir[0].CreatedBy, "CMD") {
+		st := strings.Split(hir[0].CreatedBy, "CMD [\"")
+		st = strings.Split(st[1], "\"]")
+		prog = st[0]
+	}
+
+	return prog, err
+}
+
 // ExtractFromDockerImage creates a package by extracting an executable and its shared libraries
 func ExtractFromDockerImage(imageName string, packageName string, parch string, targetExecutable string, quiet bool, verbose bool, copyWholeFS bool, args []string) (string, string) {
 	var err error
@@ -37,41 +83,7 @@ func ExtractFromDockerImage(imageName string, packageName string, parch string, 
 		packageName = strings.TrimRight(name+"_"+version, "_")
 	}
 
-	script := fmt.Sprintf(`{
-		colors=""
-
-		read_libs() {
-			for lib in $(echo "$(ldd "$1" | rev | cut -d' ' -f2 | rev)"); do
-				if [ "$(echo $lib | cut -c1-1)" = "/" ]; then
-					exists=0
-					resolved_lib=$(readlink -f $lib)
-
-					for i in $(echo "$colors"); do
-						if [ "$i" = "'$lib'" ] || [ "$i" = "'$resolved_lib'" ]; then
-							exists=1
-							break
-						fi
-					done
-
-					if [ "$exists" = "0" ]; then
-						echo "$resolved_lib => $lib"
-						colors="$colors '$lib'"
-
-						read_libs "$resolved_lib"
-					fi
-				fi
-			done
-		}
-
-		app="$(command -v "%s")"
-		echo "$app"
-		# skip statically linked binaries
-		if ! ldd "$app" 2>&1 | grep -q "Not a valid dynamic program"; then
-			read_libs "$app"
-		fi
-	}`, targetExecutable)
-
-	ctx, cli, containerInfo, err := createContainer(imageName, []string{"sh", "-c", script}, true, quiet)
+	ctx, cli, containerInfo, targetExecutable, err := createContainer(imageName, targetExecutable, true, quiet)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -191,11 +203,11 @@ func sanitizeLine(line string) string {
 	return line
 }
 
-func createContainer(image string, command []string, pull bool, quiet bool) (context.Context, *dockerClient.Client, dockerContainer.CreateResponse, error) {
+func createContainer(image string, targetExecutable string, pull bool, quiet bool) (context.Context, *dockerClient.Client, dockerContainer.CreateResponse, string, error) {
 	ctx := context.Background()
 	cli, err := dockerClient.NewClientWithOpts(dockerClient.FromEnv, dockerClient.WithAPIVersionNegotiation())
 	if err != nil {
-		return nil, nil, dockerContainer.CreateResponse{}, err
+		return nil, nil, dockerContainer.CreateResponse{}, targetExecutable, err
 	}
 
 	// grab latest if not specified
@@ -223,7 +235,7 @@ out:
 	if pull {
 		reader, err := cli.ImagePull(ctx, image, dockerTypes.ImagePullOptions{})
 		if err != nil {
-			return nil, nil, dockerContainer.CreateResponse{}, err
+			return nil, nil, dockerContainer.CreateResponse{}, targetExecutable, err
 		}
 		defer reader.Close()
 
@@ -233,16 +245,61 @@ out:
 		}
 	}
 
+	// try to look up the CMD
+	// we have to do this after the pull if it doesn't exist yet
+	if targetExecutable == "" {
+		targetExecutable, err = getCMDExecutable(image)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	script := fmt.Sprintf(`{
+		colors=""
+
+		read_libs() {
+			for lib in $(echo "$(ldd "$1" | rev | cut -d' ' -f2 | rev)"); do
+				if [ "$(echo $lib | cut -c1-1)" = "/" ]; then
+					exists=0
+					resolved_lib=$(readlink -f $lib)
+
+					for i in $(echo "$colors"); do
+						if [ "$i" = "'$lib'" ] || [ "$i" = "'$resolved_lib'" ]; then
+							exists=1
+							break
+						fi
+					done
+
+					if [ "$exists" = "0" ]; then
+						echo "$resolved_lib => $lib"
+						colors="$colors '$lib'"
+
+						read_libs "$resolved_lib"
+					fi
+				fi
+			done
+		}
+
+		app="$(command -v "%s")"
+		echo "$app"
+		# skip statically linked binaries
+		if ! ldd "$app" 2>&1 | grep -q "Not a valid dynamic program"; then
+			read_libs "$app"
+		fi
+	}`, targetExecutable)
+
+	command := []string{"sh", "-c", script}
+
 	containerInfo, err := cli.ContainerCreate(ctx, &dockerContainer.Config{
 		Image:      image,
 		Cmd:        command,
 		Entrypoint: []string{},
 	}, nil, nil, nil, "")
 	if err != nil {
-		return nil, nil, dockerContainer.CreateResponse{}, err
+		return nil, nil, dockerContainer.CreateResponse{}, targetExecutable, err
 	}
 
-	return ctx, cli, containerInfo, nil
+	return ctx, cli, containerInfo, targetExecutable, nil
 }
 
 func copyFromContainer(cli *dockerClient.Client, containerID string, containerPath string, hostPath string) error {
