0302cc42c02c26b941a4316244d86cbc8d97e111
Ian Eyberg <ian@nanovms.com>
2026-01-20 17:17:49 -0800

    .

--- provider/oci/oci_image_test.go
+++ provider/oci/oci_image_test.go
@@ -122,7 +122,7 @@ func TestListImages(t *testing.T) {
 	ctx := lepton.NewContext(lepton.NewConfig())
 
 	computeService.EXPECT().
-		ListImages(context.TODO(), core.ListImagesRequest{CompartmentId: types.StringPtr("")}).
+		ListImages(context.TODO(), core.ListImagesRequest{OperatingSystem: types.StringPtr("Custom"), CompartmentId: types.StringPtr("")}).
 		Return(core.ListImagesResponse{
 			Items: []core.Image{
 				{
