01a8a465636a5658c2c120511ee7d750c895bcfa
Ian Eyberg <ian@nanovms.com>
2026-03-10 19:57:55 -0700

    rip out tools/exec (#1760)

--- network/iproute_network_service.go
+++ network/iproute_network_service.go
@@ -3,9 +3,8 @@ package network
 import (
 	"fmt"
 	"net"
+	"os/exec"
 	"strings"
-
-	"github.com/nanovms/ops/tools"
 )
 
 // IprouteNetworkService uses iproute commands to change network configuration in OS
@@ -19,20 +18,29 @@ func NewIprouteNetworkService() *IprouteNetworkService {
 
 // AddBridge creates bridge interface
 func (s *IprouteNetworkService) AddBridge(br string) (string, error) {
-	return tools.ExecCmd(fmt.Sprintf("sudo ip link add name %s type bridge", br))
+	cmd := exec.Command("sudo", "ip", "link", "add", "name", br, "type", "bridge")
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
+
 }
 
 // ListBridges prints a list of bridge network interfaces
 func (s *IprouteNetworkService) ListBridges() (string, error) {
-	return tools.ExecCmd("sudo ip link show type bridge")
+	cmd := exec.Command("sudo", "ip", "link", "show", "type", "bridge")
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
 }
 
 // CheckBridgeHasInterface checks whether interface is listed in bridge network
 func (s *IprouteNetworkService) CheckBridgeHasInterface(bridgeName string, ifcName string) (bool, error) {
-	output, err := tools.ExecCmd(fmt.Sprintf("sudo ip link show master %s", bridgeName))
+	cmd := exec.Command("sudo", "ip", "link", "show", "master", bridgeName)
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return false, err
 	}
+	output := string(out)
 
 	ifcs := strings.Split(output, string('\t'))
 
@@ -51,10 +59,12 @@ func (s *IprouteNetworkService) CheckBridgeHasInterface(bridgeName string, ifcNa
 
 // GetBridgeInterfacesNames get list of interfaces names in bridge network
 func (s *IprouteNetworkService) GetBridgeInterfacesNames(bridgeName string) ([]string, error) {
-	output, err := tools.ExecCmd(fmt.Sprintf("sudo ip link show master %s", bridgeName))
+	cmd := exec.Command("sudo", "ip", "link", "show", "master", bridgeName)
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
+	output := string(out)
 
 	ifcs := strings.Split(output, string('\n'))
 
@@ -89,46 +99,66 @@ func (s *IprouteNetworkService) CheckNetworkInterfaceExists(name string) (bool, 
 
 // AddTap creates tap interface
 func (s *IprouteNetworkService) AddTap(tap string) (string, error) {
-	return tools.ExecCmd(fmt.Sprintf("sudo ip tuntap add %s mode tap", tap))
+	cmd := exec.Command("sudo", "ip", "tuntap", "add", tap, "mode", "tap")
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
 }
 
 // AddTapToBridge adds tap interface to bridge network
 func (s *IprouteNetworkService) AddTapToBridge(br, tap string) (string, error) {
-	return tools.ExecCmd(fmt.Sprintf("sudo ip link set %s master %s", tap, br))
+	cmd := exec.Command("sudo", "ip", "link", "set", tap, "master", br)
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
 }
 
 // SetNIIP sets network interface IP
 func (s *IprouteNetworkService) SetNIIP(ifc string, ip string, netmask string) (string, error) {
-	cmd := fmt.Sprintf("sudo ip address add %s/%s dev %s", ip, netmask, ifc)
-
-	return tools.ExecCmd(cmd)
+	ipn := fmt.Sprintf("%s/%s", ip, netmask)
+	cmd := exec.Command("sudo", "ip", "address", "add", ipn, "dev", ifc)
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
 }
 
 // FlushIPFromNI removes every IP assigned to network interface
 func (s *IprouteNetworkService) FlushIPFromNI(niName string) (string, error) {
-	cmd := fmt.Sprintf("sudo ip address flush dev %s", niName)
-
-	return tools.ExecCmd(cmd)
+	cmd := exec.Command("sudo", "ip", "address", "flush", "dev", niName)
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
 }
 
 // TurnNIUp turns on network interface
 func (s *IprouteNetworkService) TurnNIUp(ifc string) (string, error) {
-	return tools.ExecCmd(fmt.Sprintf("sudo ip link set dev %s up", ifc))
+	cmd := exec.Command("sudo", "ip", "link", "set", "dev", ifc, "up")
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
 }
 
 // TurnNIDown turns off network interface
 func (s *IprouteNetworkService) TurnNIDown(ifc string) (string, error) {
-	return tools.ExecCmd(fmt.Sprintf("sudo ip link set dev %s down", ifc))
+	cmd := exec.Command("sudo", "ip", "link", "set", "dev", ifc, "down")
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
 }
 
 // DeleteNIC removes network interface
 func (s *IprouteNetworkService) DeleteNIC(ifc string) (string, error) {
-	return tools.ExecCmd(fmt.Sprintf("sudo ip link delete %s", ifc))
+	cmd := exec.Command("sudo", "ip", "link", "delete", ifc)
+	out, err := cmd.CombinedOutput()
+	output := string(out)
+	return output, err
 }
 
 // IsNIUp checks whether network interface is on
 func (s *IprouteNetworkService) IsNIUp(ifcName string) (bool, error) {
-	output, err := tools.ExecCmd("sudo ip link ls up")
+	cmd := exec.Command("sudo", "ip", "link", "ls", "up")
+	out, err := cmd.CombinedOutput()
+	output := string(out)
 	if err != nil {
 		return false, err
 	}
@@ -150,10 +180,13 @@ func (s *IprouteNetworkService) IsNIUp(ifcName string) (bool, error) {
 
 // GetNetworkInterfaceIP get IP from network interface
 func (s *IprouteNetworkService) GetNetworkInterfaceIP(ifcName string) (string, error) {
-	output, err := tools.ExecCmd(fmt.Sprintf("sudo ip address show %s", ifcName))
+	cmd := exec.Command("sudo", "ip", "address", "show", ifcName)
+	out, err := cmd.CombinedOutput()
+
 	if err != nil {
 		return "", err
 	}
+	output := string(out)
 
 	parts := strings.Split(output, string(' '))
 
