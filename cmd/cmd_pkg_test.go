e18756d5cca4eb54e4eae49c307799d77350198a
Ian Eyberg <ian@nanovms.com>
2026-08-23 13:59:27 -0700

    yank live calls from tests

--- cmd/cmd_pkg_test.go
+++ cmd/cmd_pkg_test.go
@@ -101,52 +101,6 @@ func TestListPkgCommand(t *testing.T) {
 
 }
 
-func TestGetPkgCommand(t *testing.T) {
-	getPkgCmd := PackageCommands()
-
-	getPkgCmd.SetArgs([]string{"get", "eyberg/bind:9.13.4", "--arch", "amd64"})
-
-	err := getPkgCmd.Execute()
-
-	assert.Nil(t, err)
-}
-
-func TestPkgContentsCommand(t *testing.T) {
-	getPkgCmd := PackageCommands()
-
-	getPkgCmd.SetArgs([]string{"contents", "eyberg/bind:9.13.4"})
-
-	err := getPkgCmd.Execute()
-
-	assert.Nil(t, err)
-}
-
-func TestPkgDescribeCommand(t *testing.T) {
-	getPkgCmd := PackageCommands()
-
-	getPkgCmd.SetArgs([]string{"describe", "eyberg/bind:9.13.4", "--arch", "amd64"})
-
-	err := getPkgCmd.Execute()
-
-	assert.Nil(t, err)
-}
-
-func TestLoad(t *testing.T) {
-
-	getPkgCmd := PackageCommands()
-
-	program := buildNodejsProgram()
-	defer os.Remove(program)
-
-	getPkgCmd.SetArgs([]string{"load", "eyberg/node:v14.2.0", "--arch", "amd64", "-a", program})
-
-	err := getPkgCmd.Execute()
-
-	assert.Nil(t, err)
-
-	removeImage(program)
-}
-
 func TestPkgSearch(t *testing.T) {
 	searchPkgCmd := PackageCommands()
 
